cb938ddaf1e38c7757252933c8d2f3a39251525f
JuDevRo <76juanrouger@gmail.com>
2023-01-07 00:27:10 +0100

    Primer commit maquetando

---
+++ components/Entrada/Entrada.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Entrada.module.css'
+
+const Entrada = () => {
+    return(
+        <div className={styles.container}>
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+
+export default Entrada
